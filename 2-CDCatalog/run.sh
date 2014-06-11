0c7bc5e76df109c3fb19d59858598fbfeeba9d68
Sergey Yuferev <sergey.yuferev@kupibilet.ru>
2014-06-11 16:47:50 +0400

    validation schema added for cd catalog

--- 2-CDCatalog/run.sh
+++ 2-CDCatalog/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#xmllint --noout --relaxng data.rng data.xml
+xmllint --noout --relaxng data.rng data.xml
 xsltproc --stringparam year "2014" -o output.html format.xsl data.xml
 
 xsltproc -o output2.html --stringparam order "ascending" --stringparam sortparam "label" format.xsl data.xml
